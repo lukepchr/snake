7b9072f44087be2350d9a3de3e7b91d2bb5d9004
lukepchr <lukepchr@gmail.com>
2019-02-23 22:32:15 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 // work in progress!
 // this is up to the date with GitHub.
 
-const appleColour = "#EABDA8";
-const backColour = "#403551";
+const appleColour = '#EABDA8';
+const backColour = '#403551';
 let bonus = () => {
   bonusTimer = 0;
   appleX = Math.floor(Math.random() * 19);
@@ -19,7 +19,7 @@ let bonus = () => {
 let bonusTimer;
 let bonusIsThere = false;
 let appleX, appleY, lastDir, oldPos, shiftX, shiftY;
-let banner = document.getElementById("overlay");
+let banner = document.getElementById('overlay');
 let catchMe;
 const change = (c, col) => {
   // take coordinates like [3,4] and convert into a cell number 1-300 so we can speak to the divs.
@@ -32,22 +32,22 @@ let collide = () => {
 
       clearInterval(gameTime);
       killGame = true;
-      change(snake[0], "orange");
+      change(snake[0], 'orange');
 
-      shout("GAME OVER");
+      shout('GAME OVER');
 
       setTimeout(function() {
         change(snake[0], snakeColour);
-        shout(pts + " points!");
+        shout(pts + ' points!');
         if (gameOver)setTimeout(function() {
-          shout("Again? Press space", true);
-          space= true;
-          change(snake[0], "orange");
+          shout('Again? Press space', true);
+          space = true;
+          change(snake[0], 'orange');
           setTimeout(function() {
             change(snake[0], snakeColour);
 
             setTimeout(function() {
-              if(space) change(snake[0], "orange");
+              if (space) change(snake[0], 'orange');
               setTimeout(function() {
                 change(snake[0], snakeColour);
               }, 600);
@@ -70,16 +70,16 @@ function move() {
   shiftX = snake[0][0];
   shiftY = snake[0][1];
   switch (direction) {
-    case "n":
+    case 'n':
       shiftY = snake[0][1] - 1;
       break;
-    case "s":
+    case 's':
       shiftY = snake[0][1] + 1;
       break;
-    case "w":
+    case 'w':
       shiftX = snake[0][0] - 1;
       break;
-    case "e":
+    case 'e':
       shiftX = snake[0][0] + 1;
       break;
   }
@@ -93,17 +93,17 @@ function move() {
   if (snake[0][0] > 19) snake[0][0] = 0;
 
   collide();
-  if(!killGame) change(snake[0], snakeColour); // Draw the new head.
+  if (!killGame) change(snake[0], snakeColour); // Draw the new head.
 
   if (!gameOver) change(snake.pop(), backColour);
   // ate an apple.
 
   if (appleX == snake[0][0] && appleY == snake[0][1]) {
     snake.push(snake[snake.length - 1]);
-    change([appleX, appleY], "#517F77"); //snakeColour but darker
+    change([appleX, appleY], '#517F77'); //snakeColour but darker
     appleX = null;
     appleY = null;
-    shout("GOOD JOB!");
+    shout('GOOD JOB!');
     pts += 50;
     bonusIsThere = false;
     bonusTimer = 0;
@@ -115,48 +115,48 @@ let mPts = 0;
 let output = [];
 let pts;
 let snake = [];
-const snakeColour = "#61988E";
+const snakeColour = '#61988E';
 let speed;
 let space = false;
 const setup = () => {
   appleX, appleY, lastDir, oldPos, shiftX, shiftY = null;
-  output=[];
-for (a = 1; a <= 200; a++) {
-  output.push(`<div id="${a}" class="brick"></div>`);
-}
-document.getElementById("platform").innerHTML = output.join("");
-document.addEventListener("keydown", turning); // reads the keyboard arrows on keydown event.
-bonusTimer= 0;
+  output = [];
+  for (a = 1; a <= 200; a++) {
+    output.push(`<div id="${a}" class="brick"></div>`);
+  }
+  document.getElementById('platform').innerHTML = output.join('');
+  document.addEventListener('keydown', turning); // reads the keyboard arrows on keydown event.
+  bonusTimer = 0;
   bonusIsThere = false;
   speed = 470;
   catchMe = 10;
-snake= [];
-snake = [[10,6], [10, 7], [10, 8], [10, 9]];
-change(snake[0], snakeColour);
-change(snake[1], snakeColour);
-change(snake[2], snakeColour);
-change(snake[3], snakeColour);
-direction = "n";
-  pts= 0;
-  mPts= 0;
+  snake = [];
+  snake = [[10,6], [10, 7], [10, 8], [10, 9]];
+  change(snake[0], snakeColour);
+  change(snake[1], snakeColour);
+  change(snake[2], snakeColour);
+  change(snake[3], snakeColour);
+  direction = 'n';
+  pts = 0;
+  mPts = 0;
   speed = 450;
   killGame = false;
-} // basic setup before the game starts. Draw all <divs> and put the snake there.
+}; // basic setup before the game starts. Draw all <divs> and put the snake there.
 function shout(string, remain) {
 
-  banner.style.visibility = "visible";
+  banner.style.visibility = 'visible';
   banner.innerHTML = string.toUpperCase();
 
   setTimeout(function() {
-    banner.style.visibility = "hidden";
+    banner.style.visibility = 'hidden';
     setTimeout(function() {
-      if(gameOver) banner.style.visibility = "visible";
+      if (gameOver) banner.style.visibility = 'visible';
       setTimeout(function() {
-        banner.style.visibility = "hidden";
+        banner.style.visibility = 'hidden';
         if (remain) {
           setTimeout(function() {
 
-          if (gameOver)banner.style.visibility = "visible";
+            if (gameOver)banner.style.visibility = 'visible';
 
           }, 300);
         }
@@ -173,7 +173,7 @@ let snakeContains = () => {
   return false;
 }; // this is to check if the new apple coordinates point to a part of the snake.
 let points = () => {
-  document.getElementById("number").innerHTML = pts;
+  document.getElementById('number').innerHTML = pts;
   mPts++;
   bonusTimer++;
 
@@ -191,7 +191,7 @@ let points = () => {
     // apples disappear when catchme runs out.
     if (catchMe <= 0) {
       pts -= 55;
-      shout("missed it.");
+      shout('missed it.');
       change([appleX, appleY], backColour);
       appleX = null;
       appleY = null;
@@ -213,26 +213,24 @@ let points = () => {
 function turning(command) {
   // read the key code and let the head the right direction
   // just make sure no one tries reversing. No cheating!
-  if (command.keyCode == 37 && lastDir != "e") {
-    return (direction = "w");
-  } else if (command.keyCode == 38 && lastDir != "s") {
-    return (direction = "n");
-  } else if (command.keyCode == 39 && lastDir != "w") {
-    return (direction = "e");
-  } else if (command.keyCode == 40 && lastDir != "n") {
-    return (direction = "s");
-  }
-  else if (command.keyCode == 32 && space) {
-     // after the space was pressed, restart entire game and reset variables.
+  if (command.keyCode == 37 && lastDir != 'e') {
+    return (direction = 'w');
+  } else if (command.keyCode == 38 && lastDir != 's') {
+    return (direction = 'n');
+  } else if (command.keyCode == 39 && lastDir != 'w') {
+    return (direction = 'e');
+  } else if (command.keyCode == 40 && lastDir != 'n') {
+    return (direction = 's');
+  } else if (command.keyCode == 32 && space) {
+    // after the space was pressed, restart entire game and reset variables.
     setup();
-    banner.style.visibility = "hidden";
+    banner.style.visibility = 'hidden';
     speed = 470;
     space = false;
 
-
-    setTimeout(function(){
-      gameOver= false;
-      gameTime = setInterval(move, speed)
+    setTimeout(function() {
+        gameOver = false;
+        gameTime = setInterval(move, speed);
       }, 200);
 
   }
